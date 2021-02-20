fc6b22587415999c0658b03b298102675119c2a9
Grace Chung <hjngy0511@gmail.com>
2021-02-20 10:21:22 +0900

    route, controller 작성

---
+++ routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router(); //express.Router 클래스를 이용해서 Router 모듈로 작성하기
+const productController = require('./controller/products');
+
+//Route 정의
+router.post('/', productController.createProduct);
+router.get('/', productController.getProducts);
+router.get('/:productId', productController.getProductById);
+router.put('/:productId', productController.updateProduct);
+router.delete('/:productId', productController.deleteProduct);
+
+module.exports = router; //기본앱(server.js)에서 사용할 수 있도록 export
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require(express);
+const productRoutes = require('./routes');
 
 //Constants
 const PORT = 8080;
@@ -6,6 +7,9 @@ const HOST = '0.0.0.0';
 
 //APP
 const app = express();
+app.use(express.json()); //NOTE: bodyParser대신 express 4.16.0 버전 부터는 express에 들어있는 내장 미들웨어 함수로 bodyParser 모듈을 대체해 줄 수 있다.
+app.use('/api/products/', productRoutes);
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
