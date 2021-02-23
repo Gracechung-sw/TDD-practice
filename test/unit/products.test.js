bbdf28c000ef4d72493b9fe2f073ee398563bb9e
Grace Chung <hjngy0511@gmail.com>
2021-02-24 05:20:56 +0900

    node-mocks-http 모듈을 사용하여서 create시 mock데이터가 인자로 주어지면서 create 테스트

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,5 +1,7 @@
 const productController = require('../../controller/products');
 const productModel = require('../../models/Product');
+const httpMocks = require('node-mocks-http');
+const newProduct = require('../data/new-product.json');
 
 //Mock함수 생성
 productModel.create = jest.fn();
@@ -12,8 +14,14 @@ describe('Product Controller Create', () => {
 
   it('should call Product.create', () => {
     //createProduct 함수를 호출할 때 Product Model의 Create 메소드가 호출이 되는지를 확인 해주기 위한 테스트 코드
-    productController.createProduct(); //productController.createProduct이 호출 될 때,
-    expect(productModel.create).toBeCalled(); //productModel.create가 호출되는지
+    let req = httpMocks.createRequest();
+    let res = httpMocks.createResponse();
+    let next = null;
+    // Mock 데이터 넣어주기
+    req.body = newProduct;
+
+    productController.createProduct(req, res, next); //productController.createProduct이 호출 될 때,
+    expect(productModel.create).toBeCalledWith(newProduct);
     //그런데 이 테스트에서는 productModel에 의존적이어서는 안된다. 그래서 Mock함수를 사용한다.
   });
 });
